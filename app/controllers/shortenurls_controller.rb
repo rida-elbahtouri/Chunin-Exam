a2644300c6b336dc3fff81fb7dd1be464897ff31
rida-elbahtouri <elbahtouri0123@gmail.com>
2021-04-27 16:20:04 +0000

    create method to redirect users to original urls

--- app/controllers/shortenurls_controller.rb
+++ app/controllers/shortenurls_controller.rb
@@ -3,12 +3,20 @@ class ShortenurlsController < ApplicationController
         @shortenurl = Shortenurl.new
     end
     def create
-        url =  params.require(:shortenurl).permit(:originalUrl)
+        url =  params[:shortenurl][:originalUrl]
         @newurl = Shortenurl.new(originalUrl: url,shortUrl: generate_shorturl())
         if @newurl.save
             flash[:success] = "short url has been created"
         end
     end
    
-    
+    def goto
+        @orurl = Shortenurl.find_by(shortUrl: params[:url])
+        
+        if @orurl
+            redirect_to(URI.escape(@orurl.originalUrl))
+        else
+            #render 404
+        end
+    end
 end
